7b95ddc5596e4f84419556e1aa2724f7feee7b4d
Kingsman-Key <15077877013@163.com>
2024-03-23 11:18:19 +0800

    add geepack tidier

--- R/detectTargetLevels.R
+++ R/detectTargetLevels.R
@@ -1,6 +1,6 @@
 #' detect target levels
 #'
-#' @param target your formula
+#' @param target your target exposure
 #' @param data your data
 #' @export
 detectTargetLevels <- function(target,data){
@@ -13,5 +13,26 @@ detectTargetLevels <- function(target,data){
   }else if(is.factor(data[[target]])){
     n2 <- levels(data[[target]])
     return(n2)
+  }
+}
+
+
+
+
+#' detect outcome levels
+#'
+#' @param outcome your outcome
+#' @param data your data
+#' @export
+detectOutcomeLevels <- function(outcome,data){
+  if(is.numeric(data[[outcome]])){
+    outcomeCategory <- "continuous"
+    return(outcomeCategory)
+  }else if(is.character(data[[outcome]])){
+    outcomeCategory <- "categorical"
+    return(outcomeCategory)
+  }else if(is.factor(data[[outcome]])){
+    outcomeCategory <- "categorical"
+    return(outcomeCategory)
   }
 }
---
+++ demo/geepack-tidier_demo.R
@@ -0,0 +1,6 @@
+data(package = "geepack", dietox)
+dietox$Cu     <- as.factor(dietox$Cu)
+mf <- formula(Weight ~ Cu * (Time + I(Time^2) + I(Time^3))) %>% as.formula
+gee1 <- geepack::geeglm(mf, data=dietox, id=Pig, family=poisson("identity"), corstr="ar1")
+res <- sumReg(gee1)
+
