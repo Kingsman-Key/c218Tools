d3cda940592e7dc297acf7e7b51ce8ba656ee5cc
Kingsman-Key <15077877013@163.com>
2024-03-24 08:42:29 +0800

    add save of file

--- demo/stats-glm-tidier_demo.R
+++ demo/stats-glm-tidier_demo.R
@@ -6,5 +6,6 @@ res <- sumReg(model)
 # continuous outcome
 input <- mtcars
 am.data = glm(formula = am ~ cyl + hp + wt, data = input, family = "binomial")
+table(mtcars$am)
 res <- sumGLM(am.data)
 res <- sumReg(am.data)
