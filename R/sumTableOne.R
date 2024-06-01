feb7e917ed920d80f7e488e27512cc5dded0bc15
Kingsman-Key <15077877013@163.com>
2024-06-01 19:53:08 +0800

    add test

--- R/sumTableOne.R
+++ R/sumTableOne.R
@@ -93,12 +93,16 @@ makeNamesTableOneExcel <- function(x){ # This is for table one output to excel
 #' In academic paper, only one or two lines of regression tables were shown rather than the whole table. Since we are only interested in the specific exposure. Thus, n1 stands for the line started from which we want to extract results. n2 stands for the line to which we want to extract. Normally, you do not need to change them since this package take the first independent variable in your regression model as the variable you are interested in. It will detect which line to take from the final table.
 changeLevelTwoFactor <- function(df){
   df_con <- df[stringr::str_detect(df$Variable, "mean|median"),]
-  df_con[stringr::str_detect(df$Variable, "mean"),] <- lapply(df_con[stringr::str_detect(df$Variable, "mean"),], function(x){
-    x <- stringr::str_replace(string = x, pattern = "\\(", replacement = "± ")
-    x <- stringr::str_replace(string = x, pattern = "\\)", replacement = "")
+  df_con[stringr::str_detect(df$Variable, "mean"),2:ncol(df_con)] <- lapply(df_con[stringr::str_detect(df$Variable, "mean"),2:ncol(df_con)], function(x){
+    x <- ifelse(!stringr::str_detect(x, "\\,"), stringr::str_replace(string = x, pattern = "\\(", replacement = "± "), x)
+    x <- ifelse(!stringr::str_detect(x, "\\,"),stringr::str_replace(string = x, pattern = "\\)", replacement = ""), x)
     return(x)
   })
-
+  # df_con[stringr::str_detect(df$Variable, "median"),2:ncol(df_con)] <- lapply(df_con[stringr::str_detect(df$Variable, "median"),2:ncol(df_con)], function(x){
+  #   x <- stringr::str_replace(string = x, pattern = "\\[", replacement = "(")
+  #   x <- stringr::str_replace(string = x, pattern = "\\]", replacement = ")")
+  #   return(x)
+  # })
   # rownames(df_con) <- 1:nrow(df_con)
   df_cat <- df[!stringr::str_detect(df$Variable, "mean|median"),]
   # rownames(df_cat) <- 1:nrow(df_cat)
